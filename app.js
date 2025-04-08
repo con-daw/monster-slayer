440d5e66321786ba123098b8235ee132ea1a396f
Connor Dawson <c.dawson@digital-velocity.co.uk>
2025-04-08 14:45:36 +0100

    attack button linked up to hero and monster health

--- app.js
+++ app.js
@@ -1,29 +1,57 @@
 const app = Vue.createApp({
     data() {
-        return{
+        return {
             monsterImage: './images/monsters/',
             monsters:[
                 {
                     name: 'Azure',
-                    img: 'dragon-snake'
+                    img: 'dragon-snake',
+                    attackMax: 12,
+                    attackMin: 3
                 },
                 {
                     name: 'Kraken',
-                    img: 'sea-monster'
+                    img: 'sea-monster',
+                    attackMax: 15,
+                    attackMin: 8
                 },
                 {
                     name: 'Smaug',
-                    img: 'dragon'
+                    img: 'dragon',
+                    attackMax: 17,
+                    attackMin: 6
                 },
             ],
-            currentMonster: { name: '', img: '' }
+            currentMonster: { name: '', img: '' },
+
+            heroHealth: 100,
+            monsterHealth: 100,
         }
     },
-
+    computed: {
+      monsterHealthStyle() {
+          return {width: this.monsterHealth + '%'}
+      },
+      heroHealthStyle() {
+          return {width: this.heroHealth + '%'}
+      }
+    },
     methods: {
         getMonster(){
             const randomIndex = Math.floor(Math.random() * this.monsters.length)
             this.currentMonster = this.monsters[randomIndex]
+        },
+
+        attackMonster(){
+            const attackDamage = Math.floor(Math.random() * (12 - 5)) + 5;
+            this.monsterHealth -= attackDamage;
+            this.attackPlayer();
+        },
+        attackPlayer(){
+            const attackDamage = Math.floor(
+                Math.random() * (this.currentMonster.attackMax - this.currentMonster.attackMin)
+            ) + this.currentMonster.attackMin;
+            this.heroHealth -= attackDamage;
         }
     },
 
